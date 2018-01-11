a39de89d66964a3196ab295543bb80752c02995e
lorenzstortz <lorenzstortz@outlook.de>
2018-01-11 22:46:18 +0100

    another try for travis

--- test/testAvltree.h
+++ test/testAvltree.h
@@ -1,12 +1,8 @@
-//
-// Created by S0ras on 04.01.2018.
-//
-
 #ifndef BLATT_7_AUFGABE_1_1815_TESTAVLTREE_H
 #define BLATT_7_AUFGABE_1_1815_TESTAVLTREE_H
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
-
+#include <avltree/library.h>
 
 class AvlTreeTest : public ::testing::Test {
 };
